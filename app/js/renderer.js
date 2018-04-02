34f0e6f9efa3fab014ddda0f22e6dd5287c1b229
Ijee <IjeeX.Site@gmail.com>
2018-04-02 16:21:32 +0200

    fixes + comments

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -3,11 +3,9 @@ const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
 const ipc = electron.ipcRenderer
 
-
 const fs = require('fs')
 var xl = require('excel4node');
 var dialog = require('electron').remote.dialog;
-
 
 const app = electron.remote.app
 
@@ -56,9 +54,15 @@ var vueapp = new Vue({
     tabellenEintrag: []
   },
   computed: {
+    /**
+     * The length of all current list elements.
+     */
     rowLength: function () {
       return Object.keys(this.tabellenEintrag).length + 1;
     },
+    /**
+     * Sets all inputs to disabled if if there is no list.
+     */
     disabled: function () {
       if (Object.keys(this.shipentries).length < 1) return true;
       return false;
@@ -78,6 +82,9 @@ var vueapp = new Vue({
     }
   },
   methods: {
+    /** 
+     * Creates the modal dialog for the client.
+     */
     showModal: function () {
       if (!this.modalOpen) {
         this.modalOpen = true;
@@ -105,6 +112,10 @@ var vueapp = new Vue({
         //win.webContents.openDevTools();
       }
     },
+    /**
+     * Adds the necessary css classes to display the arrow for sorting the table.
+     * @param index the index of the table column that gets the asc or dsc css class
+     */
     addClass: function (index) {
       ret = 'arrow heading' + (index + 1);
       if (this.sortKey == index && this.sortKey < 8) {
@@ -113,11 +124,19 @@ var vueapp = new Vue({
       }
       return ret;
     },
+    /**
+     * Sorts the table based on the 
+     * @param index
+     */
     sortBy: function (index) {
       this.reverse = (this.sortKey == index) ? !this.reverse : false;
       this.sortKey = index;
       this.filterList();
     },
+    /**
+     * Changes the currently selected table to the one requested.
+     * @param index the shipentries list index
+     */
     changeTable: function (index) {
       //change current data to table that is to be displayed
       this.shipInfo = this.shipentries[index].shipInfo;
@@ -126,10 +145,18 @@ var vueapp = new Vue({
       this.currentList = index;
       this.filterList();
     },
+    /**
+     * Adds a new table to the list of tables.
+     * @param shipInfo 
+     * @param entries 
+     */
     addTable: function (shipInfo, entries) {
       this.shipentries.push(new ship(shipInfo, entries));
       this.changeTable(Object.keys(this.shipentries).length - 1);
     },
+    /**
+     * removes the currently selected table.
+     */
     removeTable: function () {
       this.shipentries.splice(this.currentList, 1);
       if (this.currentList > 0) {
@@ -140,6 +167,9 @@ var vueapp = new Vue({
       }
       this.filterList();
     },
+    /**
+     * 
+     */
     filterList: function () {
       if (this.search.length > 0) {
         this.searchList = this.tabellenEintrag.filter(this.myFilter);
@@ -149,6 +179,9 @@ var vueapp = new Vue({
       }
       this.searchList.sort(this.mySorter);
     },
+    /**
+     * 
+     */
     myFilter: function (obj) {
       boo = false;
       for (e in obj) {
@@ -156,6 +189,9 @@ var vueapp = new Vue({
       }
       return boo;
     },
+    /**
+     * 
+     */
     mySorter: function (a, b) {
       sort = this.actualColumns[this.sortKey];
       x = a[sort];
@@ -167,9 +203,9 @@ var vueapp = new Vue({
       if (typeof x == 'string') return this.reverse ? y.localeCompare(x) : x.localeCompare(y);
       console.log('Big Mistake was made');
     },
-    saveTable: function (index) {
-      return;
-    },
+    /**
+     * Adds a new row for the currently selected table.
+     */
     addRow: function () {
       newRow = {};
       newRow[this.actualColumns[0]] = Object.keys(this.tabellenEintrag).length + 1;
@@ -180,7 +216,7 @@ var vueapp = new Vue({
       newRow[this.actualColumns[5]] = this.init.status;
       newRow[this.actualColumns[6]] = this.init.abgabedatum;
       newRow[this.actualColumns[7]] = this.init.notiz;
-      
+
       this.shipentries[this.currentList].tabellenEintrag.push(newRow);
       this.init = {};
       //scroll to bottom
@@ -189,19 +225,26 @@ var vueapp = new Vue({
       }, 1);
       filterList();
     },
+    /**
+     * Deletes a specific row based on the param and calls updateRowID() after.
+     * @param eintrag 
+     */
     deleteRow: function (eintrag) {
       this.tabellenEintrag.splice(eintrag.ID - 1, 1);
       this.updateRowID();
     },
+    /**
+     * Updates the displayed IDs for entries in the currently selected table.
+     */
     updateRowID: function () {
-      for (i = 1; i <= Object.keys(this.tabellenEintrag).length; i++) {
+      for (var i = 1; i <= Object.keys(this.tabellenEintrag).length; i++) {
         this.tabellenEintrag[i - 1].ID = i;
       }
       this.filterList();
     },
-    readFile: function () {
-      return;
-    },
+    /**
+     * Loads a new table into the app.
+     */
     loadFiles: function () {
       dialog.showOpenDialog({
         filters: [
@@ -218,7 +261,7 @@ var vueapp = new Vue({
         if (fileNames === undefined) {
           return;
         } else {
-          for (i = 0; i < fileNames.length; i++) {
+          for (var i = 0; i < fileNames.length; i++) {
             fs.readFile(fileNames[i], { encoding: 'utf-8', flag: 'r' }, function (err, data) {
               if (err) {
                 console.log(err);
@@ -231,8 +274,11 @@ var vueapp = new Vue({
         }
       })
     },
+    /**
+     * Exports the currently selected table as either json or xlsx to a specified path.
+     * @param index the index of the list that is going to get exported.
+     */
     exportList: function (index) {
-
       var savePath = app.getPath('home');
       date = new Date();
       filePath = path.join(savePath, date.toISOString().substring(0, 10) + '-' + this.shipentries[index].shipInfo.name + '.json');
@@ -248,6 +294,10 @@ var vueapp = new Vue({
         this.saveFile(file, this.currentList, fileExt);
       }
     },
+    /**
+     * Exports all tables that are currently in use in the app to a specified path.
+     * @param fileExt the file extension that was selected
+     */
     exportAll: function (fileExt) {
       var savePath = app.getPath('home');
       date = new Date();
@@ -260,13 +310,18 @@ var vueapp = new Vue({
         ]
       });
       if (dir[0]) {
-        for (i = 0; i < this.shipentries.length; i++) {
+        for (var i = 0; i < this.shipentries.length; i++) {
           filePath = path.join(dir[0], date.toISOString().substring(0, 10) + '-' + this.shipentries[i].shipInfo.name + '.' + fileExt);
           this.saveFile(filePath, i, fileExt);
-          console.log('test');
         }
       }
     },
+    /**
+     * Saves a file on the client based on params.
+     * @param file the selected file path
+     * @param index the index for the shipentries list 
+     * @param fileExt the file extension that was selected
+     */
     saveFile: function (file, index, fileExt) {
       if (fileExt == 'json') {
         content = JSON.stringify(this.shipentries[index]);
@@ -287,12 +342,12 @@ var vueapp = new Vue({
           numberFormat: '$#,##0.00; ($#,##0.00); -'
         });
         //this.columns.length - 1 to suppress the last column in the file output
-        for(i = 1; i < this.columns.length - 1; i++) {
-          ws.cell(1, i).string(this.columns[i]).style({ font: { bold: true}});
+        for (var i = 1; i < this.columns.length - 1; i++) {
+          ws.cell(1, i).string(this.columns[i]).style({ font: { bold: true } });
         }
-        for (i = 0; i < this.shipentries[index].tabellenEintrag.length; i++) {
-          for (j = 1; j < this.actualColumns.length; j++) {
-            entry = this.shipentries[index].tabellenEintrag[i][this.actualColumns[j]];   
+        for (var i = 0; i < this.shipentries[index].tabellenEintrag.length; i++) {
+          for (var j = 1; j < this.actualColumns.length; j++) {
+            entry = this.shipentries[index].tabellenEintrag[i][this.actualColumns[j]];
             ws.cell(i + 2, j).string('' + entry).style(style);
           }
         }
@@ -301,23 +356,34 @@ var vueapp = new Vue({
     }
   }
 })
-
+/**
+ * @constructor 
+ * @param shipInfo an object that stores name, eta, schiffsnotiz
+ * @param tableObj an object that stores the table content
+ */
 function ship(shipInfo, tableobj) {
   this.shipInfo = shipInfo;
   this.tabellenEintrag = tableobj;
 }
 
-/* 
-  - 183 is the pixel count based on the other content in maincontent
-*/
+/*
+ *  Sets the height of the css class maincontent based on resizing the window itself.
+ *  - 183 is the pixel count based on the other content in maincontent
+ */
 function setHeight() {
   vueapp.currentHeight = maincontent.clientHeight - 183 + 'px';
 }
-
+/**
+ * Calls vueapp filterList onkeyup for the search input.
+ */
 function filterList() {
   vueapp.filterList();
 }
-
+/**
+ * This function waits on information from main.js that are used in modal.html to create a new table for the app.
+ * @param event the event
+ * @param functionality a string to differentiate between the users action in the modal
+ */
 ipc.on('ship-information', function (event, functionality) {
   if (functionality == 'addTable') {
     vueapp.addTable({ name: '', eta: '', schiffsnotiz: '' }, []);
--- app/js/ship.js
+++
@@ -1,9 +0,0 @@
-class Ship {
-
-    //name, eta, shipNote, sdgNr, contNr, loeschDatum, time, status, abgabeDatum, contNote
-    constructor(name, shipETA, shipNote, tableobj) {
-        this.shipName = name;
-        this.shipETA = shipETA;
-        this.shipNote = shipNote;
-    }
-}
